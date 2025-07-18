8507217c010530862925b58670f368f8d129eca3
Shamveel P <shamveelp007@gmail.com>
2025-07-19 00:42:38 +0530

    Added Commands

--- main.py
+++ main.py
@@ -12,8 +12,15 @@ load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 BOT_USERNAME: Final = '@conv_erte_r_bot'
 
+# Commands to the bot 
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPES):
+    await update.message.reply_text('Hello! Thanks for chating with me..! I am a bot created by Coding Aashan!')
 
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPES):
+    await update.message.reply_text('Type Something so I Can Respond!')
 
+async def aashanod_command(update: Update, context: ContextTypes.DEFAULT_TYPES):
+    await update.message.reply_text('You wanna tell something to coding aashan')
 
 
 
