d042ef59f1d871f773bbe5a95e97e34827687610
Shamveel P <shamveelp007@gmail.com>
2025-07-19 00:00:02 +0530

    Initial setup 2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import Final
 import os
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import Application,CommandHandler,MessageHandler,filter, ContextTypes,ApplicationBuilder
 
 
 # Load environment variables from .env file
@@ -10,9 +10,9 @@ load_dotenv()
 
 # Access the token
 TOKEN = os.getenv("BOT_TOKEN")
-
-
 BOT_USERNAME: Final = '@conv_erte_r_bot'
+
+
 
 
 
